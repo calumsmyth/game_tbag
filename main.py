d4928e7708077902e3726c51df48025363d46cb0
calumsmyth <calumsmyth@hotmail.co.uk>
2025-01-17 11:51:50 +0000

    Changed how to define room names as name removed from Room parameters

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from room import Room
 
-kitchen = Room("kitchen")
-ballroom = Room("ballroom")
-dining_room = Room("dining room")
+kitchen = Room()
+kitchen.set_name("kitchen")
+kitchen.set_description("A dank and dirty room buzzing with flies")
 
-kitchen.set_description("A large, fancy and prestine kitchen. Grand enough to satisfy even Gordon Ramsey.")
+ballroom = Room()
+ballroom.set_name("ballroom")
+ballroom.set_description("A vast room with a shiny wooden floor")
+
+dining_room = Room()
+dining_room.set_name("dining hall")
+dining_room.set_description("A large room with ornate golden decorations")
+
+
+print(kitchen.name)
 kitchen.describe()
+
+print(ballroom.name)
+ballroom.describe()
+
+print(dining_room.name)
+dining_room.describe()
