530fad9d0f29e79422b0a5c962950c5781a53f31
calumsmyth <calumsmyth@hotmail.co.uk>
2025-01-20 17:38:43 +0000

    test page

---
+++ character_test.py
@@ -0,0 +1,13 @@
+from character import Character, Enemy
+
+dave = Enemy("Dave", "A smelly zombie")
+
+dave.describe()
+dave.set_conversation("Hello there! I am going to join your OOP game very soon")
+dave.talk()
+
+dave.set_weakness("cheese")
+
+print("What will you fight with?")
+fight_with = input()
+dave.fight(fight_with)
